a95d16ec0baf6c9254d23c7bd7eba7b25e3138bc
amandaghassaei <amandaghassaei@gmail.com>
2023-03-07 11:27:34 -0800

    cleanup

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,4 @@
 import ts from "rollup-plugin-ts";
-// import commonjs from '@rollup/plugin-commonjs';
-// import resolve from '@rollup/plugin-node-resolve';
-// import { terser } from "rollup-plugin-terser";
-// import nodePolyfills from 'rollup-plugin-polyfill-node';
 
 export default {
   input: 'src/type-checks.ts',
@@ -13,18 +9,8 @@ export default {
       format: 'umd',
       name: 'TypeChecks',
     },
-	// {
-    //   file: 'dist/origami-simulation.min.js',
-    //   sourcemap: true,
-    //   format: 'umd',
-    //   name: 'OrigamiSimulation',
-	//   plugins: [terser()],
-	// }
   ],
   plugins: [
     ts(),
-    // commonjs(),
-	// nodePolyfills(),
-    // resolve(),
   ],
 };
